428e1af7503c0cb882fbaf27e31ac4b8a387e344
David Pardo <davidpardomartin@gmail.com>
2024-02-06 20:58:01 +0100

    fetch added to todo app

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,104 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import Form from "./todoForm";
-// import TodoList from "./todoList";
-
-// const Home = () => {
-
-//   // Almaceno las tareas y utilizo un array para almacenarlas y asi tambien tener su indice.
-
-//   const [tasks, setTasks] = useState([]);
-
-//   const fetchData = () => {
-//     var requestOptions = {
-//       method: 'GET',
-//       redirect: 'follow'
-//     };
-
-//     fetch("https://playground.4geeks.com/apis/fake/todos/user/david", requestOptions)
-//       .then(response => response.json())
-//       .then(result => setTasks(result))
-//       .catch(error => console.log('error', error));
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-
-//   // Funcion para setear tasks con el setTasks, hago ... copia del array tasks y añado una nueva task al final del array.
-//   const addNewTask = (newTask) => {
-
-//     const updatedList = [...tasks, newTask]
-
-//     var myHeaders = new Headers();
-//     myHeaders.append("Content-Type", "application/json");
-
-//     var raw = JSON.stringify(updatedList)
-//       ;
-
-//     var requestOptions = {
-//       method: 'PUT',
-//       headers: myHeaders,
-//       body: raw,
-//       redirect: 'follow'
-//     };
-
-//     fetch("https://playground.4geeks.com/apis/fake/todos/user/david", requestOptions)
-//       .then(response => response.text())
-//       .then(result => fetchData())
-//       .catch(error => console.log('error', error));
-
-
-//     console.log(updatedList);
-
-//   };
-
-
-
-//   // Funcion para eliminar tareas.
-
-//   const deleteCompleteTask = (index) => {
-//     // Hago una copia de las tareas y la guardo.
-//     const updatedTasks = [...tasks];
-//     // Elimino la tarea en la posicion index de la copia.
-//     updatedTasks.splice(index, 1);
-//     // Actualizo en estado de tasks con updateTasks que era la copia guardada anteriormente.
-//     setTasks(updatedTasks);
-//   };
-
-//   return (
-//     <div>
-//       <div style={{ backgroundColor: '#eeeee' }}>
-//         <div className="container py-5 h-100">
-//           <div className="row d-flex justify-content-center align-items-center h-100">
-//             <div className="col col-lg-9 col-xl-7">
-//               <div className="card rounded-3">
-//                 <div className="card-body p-4">
-//                   <h4 className="satisfy-regular text-center my-3 pb-3">To-Do</h4>
-
-//                   {/* Traigo por Props las addTask y deleteTask */}
-
-//                   <Form addTask={addNewTask} forwardTasks={tasks} />
-//                   <TodoList tasks={tasks} deleteTask={deleteCompleteTask} />
-//                 </div>
-//                 <div className="border-bottom paper m-0 p-1"></div>
-//                 <div className="border-bottom paper m-0 p-1"></div>
-//                 <div className="border-bottom paper m-0 p-1"></div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
-
 import React, { useState, useEffect } from "react";
 import Form from "./todoForm";
 import TodoList from "./todoList";
@@ -106,13 +5,52 @@ import TodoList from "./todoList";
 const Home = () => {
 
   const [tasks, setTasks] = useState([]);
+  const [defaultUser, setDefaultUser] = useState("david11");
 
+  // Hago un useEffect con un FETCH (GET) para obtener tareas y usuario, como no hay usuario despues hago un POST.
   useEffect(() => {
-    fetch('https://playground.4geeks.com/apis/fake/todos/user/david')
-      .then(response => response.json())
-      .then(data => setTasks(data));
-  }, []);
+    const options = {
+      method: 'GET',
+      headers: { 'User-Agent': 'insomnia/8.3.0', 'Content-Type': 'application/json' }
+    };
+    // Hago un FETCH para obtener el usuario.
+    fetch(`https://playground.4geeks.com/apis/fake/todos/user/${defaultUser}`, options)
+      .then(response => {
 
+        if (!response.ok) {
+
+          throw Error(response.statusText);
+        }
+
+        return response.json()
+      })
+      .then(data => {
+        setTasks(data)
+      })
+      .catch(err => {
+
+        if (err.message.includes("NOT FOUND")) {
+
+          const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'User-Agent': 'insomnia/8.3.0' },
+            body: JSON.stringify(tasks)
+          };
+          // Hago un FETCH para crear el usuario.
+          fetch(`https://playground.4geeks.com/apis/fake/todos/user/${defaultUser}`, options)
+            .then(response => response.json())
+            .then(response => {
+              alert(response.msg);
+              window.location.reload(true);
+            })
+            .catch(err => console.error(err));
+
+        }
+        console.error(err)
+      });
+  }, [])
+
+  // Función para añadir una nueva tarea.
   const addNewTask = (task) => {
     const newTasks = [...tasks, { label: task, done: false }];
     setTasks(newTasks);
@@ -122,6 +60,7 @@ const Home = () => {
 
   };
 
+  //Función para eliminar la tarea completada, de una en una.
   const deleteCompleteTask = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
@@ -129,17 +68,40 @@ const Home = () => {
     updateTasksInServer(updatedTasks);
   };
 
+  //Función para eliminar todas las tareas, usuario y dejar el array vacío.
+  const deleteAllTasks = () => {
+    const emptyTasks = [];
+    setTasks(emptyTasks);
+    updateTasksInServer(emptyTasks);
+    alert("User & tasks has been deleted")
+  };
+
+  // Función para actualizar las tareas.
   const updateTasksInServer = (tasks) => {
-    fetch('https://playground.4geeks.com/apis/fake/todos/user/david', {
-      method: "PUT",
-      body: JSON.stringify(tasks),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(resp => resp.json())
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+    // Condicional para manejar el error cuando el array de tareas esta vacío.
+    if (tasks.length > 0) {
+      fetch(`https://playground.4geeks.com/apis/fake/todos/user/${defaultUser}`, {
+        method: "PUT",
+        body: JSON.stringify(tasks),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    } else {
+
+      fetch(`https://playground.4geeks.com/apis/fake/todos/user/${defaultUser}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    }
   };
 
   console.log("tareas despues de ejcutarse todas la funcionalidades--->", tasks)
@@ -164,6 +126,12 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        <button
+          className="btn btn-secondary"
+          onClick={deleteAllTasks}
+        >Delete all tasks</button>
       </div>
     </div>
 
